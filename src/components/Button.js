81e320abd4dacd1d0445aafb454fc4ff6de7cc09
Angela Zhu <xiaolinangela@gmail.com>
2020-03-30 13:42:19 -0700

    use context to update state

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
+
+// class Button extends Component {
+//   //contextType is a special name
+//   //below line is equilavent to Button.contextType = LanguageContext
+//   static contextType = LanguageContext;
+//   render() {
+//     const text = this.context === "english" ? "Submit" : "Voorleggen";
+//     return <button className="ui button primary">{text}</button>;
+//   }
+// }
+
+// export default Button;
 
 class Button extends Component {
-  //contextType is a special name
-  //below line is equilavent to Button.contextType = LanguageContext
-  static contextType = LanguageContext;
+  renderSubmit(value) {
+    return value === "english" ? "Submit" : "Voorleggen";
+  }
+
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {({ language }) => this.renderSubmit(language)}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
   render() {
-    const text = this.context === "english" ? "Submit" : "Voorleggen";
-    return <button className="ui button primary">{text}</button>;
+    return (
+      <ColorContext.Consumer>
+        {color => this.renderButton(color)}
+      </ColorContext.Consumer>
+    );
   }
 }
 
--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,3 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
+const Context = React.createContext("dutch");
 
-export default React.createContext("english");
+export class LanguageStore extends Component {
+  state = { Language: "English" };
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+  render() {
+    return (
+      <Context.Provider
+        value={{ ...this.state, onLanguageChange: this.onLanguageChange }}
+      >
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export default Context;
